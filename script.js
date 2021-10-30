b70a249aa3d8e5a175feec59fd08a8c9b70b7c4b
hermkan <hermann.kanga.dev@gmail.com>
2021-10-30 04:31:58 -0400

    added styles banner and navbar

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
-function changeBackgroundAfterFiveSecond() {}
-function changeBackground() {
-  setTimeout(() => {
-    changeBackgroundAfterFiveSecond();
-  }, 5000);
+const sectionOne = document.querySelector('.section-one');
+function generateNewNumber() {
+  let newNum = Math.floor(Math.random() * 5);
+  // console.log(newNum);
+  return newNum;
 }
-changeBackground();
+const bgimg = [
+  'url(./images/slideshow/section-1-bg-1.jpg)',
+  'url(./images/slideshow/section-1-bg-2.jpg)',
+  'url(./images/slideshow/section-1-bg-3.jpg)',
+  'url(./images/slideshow/section-1-bg-4.jpg)',
+  'url(./images/slideshow/section-1-bg-5.jpg)',
+];
+function changeBackg() {
+  sectionOne.style.background = `${bgimg[generateNewNumber()]}`;
+}
+
+// setInterval(() => {
+//   changeBackg();
+// }, 5000);
+
 // for (let i = 1; i <= 5; i++) {
 //   if (i === 5) {
 //     // i = 1;
