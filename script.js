8044bec2a98a81190689e09b2774a3437e2e475f
hermkan <hermann.kanga.dev@gmail.com>
2021-10-30 06:22:04 -0400

    completed slideshow functionnality

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
-const sectionOne = document.querySelector('.section-one');
-function generateNewNumber() {
-  let newNum = Math.floor(Math.random() * 5);
-  // console.log(newNum);
-  return newNum;
-}
-const bgimg = [
-  'url(./images/slideshow/section-1-bg-1.jpg)',
-  'url(./images/slideshow/section-1-bg-2.jpg)',
-  'url(./images/slideshow/section-1-bg-3.jpg)',
-  'url(./images/slideshow/section-1-bg-4.jpg)',
-  'url(./images/slideshow/section-1-bg-5.jpg)',
-];
-function changeBackg() {
-  sectionOne.style.background = `${bgimg[generateNewNumber()]}`;
-}
+// Create five elements dynamically and add them to wrapper slideshow
 
-// setInterval(() => {
-//   changeBackg();
-// }, 5000);
+const createSlideShowDivs = () => {
+  //loop from 1 to 5 and for each iteration create a div element using create element
+  for (let i = 1; i <= 5; i++) {
+    const div = document.createElement('div');
+    //assign to the newly created element a bg
+    //on the first iteration i will be equal to one
+    //on the second 2 and etc...
+    div.style.backgroundImage = `url(/images/slideshow/section-1-bg-${i}.jpg)`;
+    //add class "CHANGE" to each div on each iteration
+    //add class "CHANGE" on the first iteration
+    i === 1 && div.classList.add('change');
+    // newDiv.classList.add('change');
+    //also add the div element to the wrapper slideshow
+    document.querySelector('.slideshow').appendChild(div);
+    //inspect the page to verify
+  }
+};
+createSlideShowDivs();
+// add and remove class change to the div element in order
+// select all the newdiv elements
+const divs = document.querySelectorAll('.slideshow div');
 
-// for (let i = 1; i <= 5; i++) {
-//   if (i === 5) {
-//     // i = 1;
-//     // console.log(i);
-//   }
-// }
+let a = 1;
+const createslideShow = () => {
+  setInterval(() => {
+    a++;
+    const div = document.querySelector('.slideshow .change');
+    // console.log(div);
+    div.classList.remove('change');
+    if (a < divs.length) {
+      div.nextElementSibling.classList.add('change');
+    } else {
+      divs[0].classList.add('change');
+      a = 1;
+    }
+  }, 1000);
+};
+// createslideShow();
