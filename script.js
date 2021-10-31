84c585ea0061ee318c0cca88d64b3b8fd4fda1f8
hermkan <hermann.kanga.dev@gmail.com>
2021-10-31 17:38:28 -0400

    completed functionality watches

--- script.js
+++ script.js
@@ -49,3 +49,68 @@ window.addEventListener('scroll', () => {
     sectionthreeContent.classList.add('change');
   }
 });
+const watchControl = document.querySelector('.watch-control');
+
+watchControl.addEventListener('click', (e) => {
+  e.preventDefault();
+});
+const watchBands = document.querySelector('.watch-bands');
+const watchCases = document.querySelector('.watch-cases');
+
+const watchTopControl = document.querySelector('.watch-top-control');
+const watchRightControl = document.querySelector('.watch-right-control');
+const watchBottomControl = document.querySelector('.watch-bottom-control');
+const watchLeftControl = document.querySelector('.watch-left-control');
+
+let axisY = 0;
+let axisX = 0;
+
+const hideControl = () => {
+  if (axisY === -280) {
+    watchTopControl.classList.add('hideControl');
+  } else {
+    watchTopControl.classList.remove('hideControl');
+  }
+
+  if (axisY === 280) {
+    watchBottomControl.classList.add('hideControl');
+  } else {
+    watchBottomControl.classList.remove('hideControl');
+  }
+
+  if (axisX === 280) {
+    watchRightControl.classList.add('hideControl');
+  } else {
+    watchRightControl.classList.remove('hideControl');
+  }
+
+  if (axisX === -280) {
+    watchLeftControl.classList.add('hideControl');
+  } else {
+    watchLeftControl.classList.remove('hideControl');
+  }
+};
+
+watchTopControl.addEventListener('click', (e) => {
+  watchCases.style.marginTop = `${(axisY -= 70)}rem`;
+  hideControl();
+  e.preventDefault();
+});
+
+watchBottomControl.addEventListener('click', (e) => {
+  watchCases.style.marginTop = `${(axisY += 70)}rem`;
+  hideControl();
+  e.preventDefault();
+});
+
+watchRightControl.addEventListener('click', (e) => {
+  watchBands.style.marginRight = `${(axisX += 70)}rem`;
+  hideControl();
+  e.preventDefault();
+});
+
+watchLeftControl.addEventListener('click', (e) => {
+  watchBands.style.marginRight = `${(axisX -= 70)}rem`;
+  hideControl();
+  e.preventDefault();
+});
